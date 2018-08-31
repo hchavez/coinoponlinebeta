9c262c823c893d4644e7c058892c81420c8fca5c
ASCENTRI04\Chris <cmyal02@gmail.com>
2018-08-31 10:23:59 +0800

    managing agents under site

---
+++ app/ManagingAgent.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ManagingAgent extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'managing_agents';
+    public $timestamps = false;
+
+
+    /**
+    * The attributes that aren't mass assignable.
+    *
+    * @var array
+    */
+    protected $guarded = [];
+}
