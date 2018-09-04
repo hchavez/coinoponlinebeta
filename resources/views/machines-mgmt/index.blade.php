9110ff2f55bf5170e46dd5c2fd86d673e08e5507
ASCENTRI04\Chris <cmyal02@gmail.com>
2018-09-04 15:45:34 +0800

    Machine list filter fixes

--- resources/views/machines-mgmt/index.blade.php
+++ resources/views/machines-mgmt/index.blade.php
@@ -23,14 +23,36 @@
                 <?php echo (!empty($data['dateRange']))? '<code>Filtered Date: '.$data['dateRange'].'</code>' : ''; ?>               
             </form>
             <br>
-            <div class="row"><div class="col-sm-12 longFilter" style="padding:0;">     
-                     
-                    <button type="submit" id="clearFilter" class="btn btn-danger">Clear Filter</button>
-                    <button type="button" class="btn btn-outline btn-info"  id="filterBy">Filter By</button>                    
-                    <div id="filterDiv"></div>
-                        <table class="display table table-hover dataTable table-striped w-full dtr-inline table-responsive" id="dashboard_sort" role="grid" aria-describedby="exampleTableSearch_info" cellspacing="0" width="100%">
+            <div class="row">                
+                <div class="col-md-12 text-right"><button type="button" class="btn btn-danger" id="clear_filter">Clear Filter</button></div>
+                <div class="col-sm-12 longFilter" style="padding:0;">  
+                    <!--button type="submit" id="clearFilter" class="btn btn-danger">Clear Filter</button>
+                    <button type="button" class="btn btn-outline btn-info"  id="filterBy">Filter By</butto-->                    
+                    <div id="filterDiv"></div>                        
+                        <table class="table table-striped table-bordered display" id="machine_list_table" role="grid" aria-describedby="exampleTableSearch_info" cellspacing="0" width="100%">
                             <thead>
                             <tr role="row">
+                                <th>Category</th>                                
+                                <!--th>Type</th-->
+                                <th>Model</th>
+                                <th>Serial No</th>
+                                <th>Site</th>
+                                <th>State</th>
+                                <th>Route</th>
+                                
+                                <th>Area</th>
+                                <th>Comments</th>                                
+                                <th>Total Money </th>	
+                                <th>Toys won </th>	
+                                <th>Stock left </th>
+                                <th>Slip Voltage </th>
+                                <th>PK Volt </th>	
+                                <th>RET Volt </th>	
+                                <th>Owed Win </th>	
+                                <th>Excess win  </th>	
+                                <th>Activity  </th>
+                            </tr>  
+                            <tr role="row" id="table_header_filter">
                                 <th>Category</th>                                
                                 <!--th>Type</th-->
                                 <th>Model</th>
@@ -48,11 +70,8 @@
                                 <th>RET Volt </th>	
                                 <th>Owed Win </th>	
                                 <th>Excess win  </th>	
-<!--                                <th>Error</th>-->
                                 <th>Activity  </th>	
-<!--                                <th>Sync Status</th>
-                                <th>Active</th>-->
-                            </tr>                            
+                            </tr>  
                             </thead>
                             <tbody>
                             @foreach ($machines as $machine)                            
@@ -74,11 +93,8 @@
                                     <td>{{ $machine->pkup_volt }}</td>
                                     <td>{{ $machine->ret_volt }}</td>
                                     <td>{{ $machine->owed_win }}</td>
-                                    <td>{{ $machine->excess_win }}</td>                                
-                                    <!--<td></td>-->
+                                    <td>{{ $machine->excess_win }}</td>    
                                     <td>{{date('d/m/Y h:i A', strtotime($machine->last_played))}}</td>
-<!--                                    <td></td>                                         
-                                    <td> <?php //if($machine->status == '1') {$active = "Yes"; }else{ $active = "No"; }  ?><?php //echo $active; ?> </td>-->
                                 </tr>                            
                             @endforeach 
                             </tbody>
@@ -93,8 +109,52 @@
 <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
 <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+<style> 
+.select2-container{width:100% !important;}
+.select2-container .select2-choice > .select2-chosen{color:#333 !important;}
+div.dataTables_wrapper div.dataTables_filter { text-align: left !important; }
+</style>
 <script>    
 $(document).ready(function() {   
+    $('#machine_list_table').DataTable( {
+        pageLength: 15,
+        dom: 'Bfrtip',
+        buttons: ['excelHtml5'],
+        initComplete: function () {
+            this.api().columns().every( function () {
+                var column = this;
+                var select = $('<select><option value=""></option></select>')
+                    .appendTo( $(column.header()).empty() )
+                    .on( 'change', function () {
+                        var val = $.fn.dataTable.util.escapeRegex(
+                            $(this).val()
+                        );
+ 
+                        column
+                            .search( val ? '^'+val+'$' : '', true, false )
+                            .draw();
+                    } );
+ 
+                column.data().unique().sort().each( function ( d, j ) {
+                    select.append( '<option value="'+d+'">'+d+'</option>' )
+                } );
+            } );
+        }
+    });
+    //Filter customization   
+    $('#machine_list_table select').each(function(i) {
+        var label = ['Category', 'Model', 'Serial No', 'Site', 'State', 'Route','Area','Comments','Total Money','Toys Won','Stock left','Slip Voltage','PK Volt','RET Volt','Owed Win','Excess Win','Activity'];
+        $(this).attr('id', 'filter'+(i+1));        
+        $("#filter" + (i+1)).select2({
+            //placeholder: label[i]
+        });        
+    });
+    //Clear filter 
+    $('#clear_filter').click(function(){
+        $('select').val('').change();
+    });
+    
+    
     /*Resolve*/
     $('input[name="dateRange"]').daterangepicker({
         autoUpdateInput: false,
@@ -110,7 +170,8 @@ $(document).ready(function() {
         $(this).val('');
         var select = $(this), form = select.closest('form'); form.attr('action', 'machine-management'); form.submit();
     });
-    
+    $('#machine_list_table_wrapper .buttons-excel').css('float','inherit');
+    $('#machine_list_table_wrapper .buttons-excel').insertAfter('#machine_list_table_filter label');
 });
 </script>
 
