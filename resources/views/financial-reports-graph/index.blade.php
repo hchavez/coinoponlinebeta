394952b8505ae6fed8521d07da342a9b248a77a5
Harreyson Choi Chavez <harreyson.chavez@gmail.com>
2019-04-03 14:24:35 +0800

    data graph fix date

--- resources/views/financial-reports-graph/index.blade.php
+++ resources/views/financial-reports-graph/index.blade.php
@@ -101,8 +101,25 @@ function createChart() {
         },
         tooltip: {              
             formatter: function() {                                
+                var addmonth = 1;
                 var millis = new Date(this.x);
-                var theDate = millis.getUTCDate() +'-'+ millis.getUTCMonth() +'-'+ millis.getUTCFullYear();
+                var month = new Array(12);
+                    month[0] = "01";
+                    month[1] = "022";
+                    month[2] = "03";
+                    month[3] = "04";
+                    month[4] = "05";
+                    month[5] = "06";
+                    month[6] = "07";
+                    month[7] = "08";
+                    month[8] = "09";
+                    month[9] = "10";
+                    month[10] = "11";
+                    month[11] = "12";
+
+                    var n = month[millis.getUTCMonth()];
+  
+                var theDate = millis.getUTCDate() +'-'+ n +'-'+ millis.getUTCFullYear();
                 var tooltip='<b>Date: '+theDate+'<b><br/>';
                 $.each(this.points,function(i,point){
                     tooltip+= point.series.name+': '+point.y+'<br/>'
